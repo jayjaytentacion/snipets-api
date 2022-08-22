b9166fd8591bf41df9244353890a7c148411de23
okputu julius <juliusstan10@gmail.com>
2022-08-22 23:42:52 +0100

    add:docs ,refactor:views

--- snippetsapi/urls.py
+++ snippetsapi/urls.py
@@ -1,9 +1,30 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from snippetsapi import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(),name='snippets'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(),name='snippets_detail'),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', include(router.urls)),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- snippetsapi/views.py
+++ snippetsapi/views.py
@@ -4,26 +4,35 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippetsapi.permissions import IsOwnerOrReadOnly
-
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    # """
+    # This viewset automatically provides `list`, `create`, `retrieve`,
+    # `update` and `destroy` actions.
+
+    # Additionally we also provide an extra `highlight` action.
+    # """
+    queryset=Snippet.objects.all()
+    serializer_class=SnippetSerializer
     permission_classes=[permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
-class UserList(generics.ListAPIView):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-
+    @action(detail=True,renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self,request,*args,**kwargs):
+        snippet=self.get_object()
+        return Response(snippet.highlighted)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)    
